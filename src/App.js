d35ae6f3d7b95bb2ad65f3363a2487e850310a0a
theskimless <theskimless@gmail.com>
2019-09-28 12:27:30 +0300

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MenuView from "./components/Menu/MenuView";
 import WordsContainer from "./components/Words/WordsContainer";
 import ProfileView from "./components/Profile/ProfileView";
 import NotificationContainer from "./components/Notification/NotificationContainer";
+import axios from "axios";
 
 function App() {
   return (
@@ -21,6 +22,12 @@ function App() {
       <Route path="/profile" render={() => <ProfileView />} />
 
       <NotificationContainer width="576" />
+
+      <button onClick={() => {
+        axios.post("https://127.0.0.1:5000/test", {token: localStorage.getItem("access_token")})
+          .then(res => console.log(res));
+
+      }}>TEST</button>
     </div>
   );
 }
